2e0203cfe54b01d54c40ba56690dc113276d5148
yoursram2k25-create <yoursram2k25@gmail.com>
2026-03-15 12:01:09 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request, redirect
+import psycopg2
+
+app = Flask(__name__)
+
+conn = psycopg2.connect(
+    host="YOUR_HOST",
+    database="postgres",
+    user="postgres",
+    password="YOUR_PASSWORD",
+    port="5432"
+)
+
+@app.route("/")
+def index():
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM products")
+    data = cur.fetchall()
+    cur.close()
+    return render_template("index.html", products=data)
+
+@app.route("/add", methods=["POST"])
+def add():
+    name = request.form["name"]
+    qty = request.form["qty"]
+    price = request.form["price"]
+    supplier = request.form["supplier"]
+
+    cur = conn.cursor()
+    cur.execute(
+        "INSERT INTO products (name, quantity, price, supplier) VALUES (%s,%s,%s,%s)",
+        (name, qty, price, supplier)
+    )
+    conn.commit()
+    cur.close()
+
+    return redirect("/")
+
+app.run()
